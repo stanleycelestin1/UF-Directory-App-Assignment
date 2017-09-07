231772b5969ac492c3472fb1d6bffc133d2832d0
Stanley Celestin <stanleycelestin1@gmail.comgit config --global user.namegit config --global user.email stanleycelestin1@gmail.com>
2017-09-06 23:49:45 -0400

    .5 L

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,14 +7,52 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config'),
+    ;
+
+
+var listingSchema = new Schema({
+  /* your code here */
+  code: {type: String, required: true},
+  name: {type: String, required: true},
+  coordinates: { latitude: Number, longitude: Number },
+  address: String
+});
 
 /* Connect to your database */
-
+mongoose.connect(db.uri);
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
+
+var listingsData;
+
+fs.readFile('listings.json', 'utf8', function(err, data) {
+  /*
+    This callback function should save the data in the listingData variable, 
+    then start the server. 
+   */
+   if (err){
+    throw err;
+   }
+
+   listingData = data.entries;
+});
+
+for(i=0; i<listingsData.length;i++){
+
+  var newListing = Listing({
+    code: listingsData[i].code,
+    name: listingsData[i].name,
+    coordinates: { latitude: listingsData[i].coordinates.latitude,
+      longitude: listingsData[i].coordinates.longitude },
+    address: listingsData[i].address
+  });
+
+
+}
+
 
 
 /* 
